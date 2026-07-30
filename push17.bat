c820387abf7a1f4c99adc0c1d8b9f74cab2a4166
kangmeddia-coder <kangmeddia-coder@users.noreply.github.com>
2026-07-30 23:02:48 +0700

    Add AUTH_SECRET and AUTH_TRUST_HOST to wrangler.jsonc

---
+++ push17.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Git\cmd\git.exe" add -A
+"C:\Program Files\Git\cmd\git.exe" commit -m "Add AUTH_SECRET and AUTH_TRUST_HOST to wrangler.jsonc"
+"C:\Program Files\Git\cmd\git.exe" push
