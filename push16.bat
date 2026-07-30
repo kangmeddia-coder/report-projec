66556be4cdd2d93c7bb31cb115286e0c8a30b9a5
kangmeddia-coder <kangmeddia-coder@users.noreply.github.com>
2026-07-30 22:53:58 +0700

    Fix WORKER_SELF_REFERENCE service name in wrangler.jsonc

---
+++ push16.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Git\cmd\git.exe" add -A
+"C:\Program Files\Git\cmd\git.exe" commit -m "Fix WORKER_SELF_REFERENCE service name in wrangler.jsonc"
+"C:\Program Files\Git\cmd\git.exe" push
