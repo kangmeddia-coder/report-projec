646c98fda849f79f3a62d5dae12319f5eab27347
kangmeddia-coder <kangmeddia-coder@users.noreply.github.com>
2026-07-30 22:31:24 +0700

    Use OpenNext migrate-generated config - correct setup

---
+++ push14.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Git\cmd\git.exe" add -A
+"C:\Program Files\Git\cmd\git.exe" commit -m "Use OpenNext migrate-generated config - correct setup"
+"C:\Program Files\Git\cmd\git.exe" push
